81fce6ae6535e1439299695bee0d3d08b7da718e
Shlomi Fish <shlomif@shlomifish.org>
2022-04-10 05:42:24 +0300

    prepare for a release

--- lib/HTML/T5.pm
+++ lib/HTML/T5.pm
@@ -13,11 +13,11 @@ HTML::T5 - HTML validation in a Perl object
 
 =head1 VERSION
 
-Version 0.012
+Version 0.013
 
 =cut
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 =head1 SYNOPSIS
 
--- lib/Test/HTML/T5.pm
+++ lib/Test/HTML/T5.pm
@@ -24,11 +24,11 @@ Test::HTML::T5 - Test::More-style wrapper around HTML::T5
 
 =head1 VERSION
 
-Version 0.012
+Version 0.013
 
 =cut
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 my $TB = Test::Builder->new;
 
